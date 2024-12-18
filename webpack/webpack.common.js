53be4c107e000929f1271309ed95324ebe57dd0e
Anton Zhilenkov <a.zhilenkov@globexit.ru>
2024-12-18 23:08:29 +0300

    chore: webpack init

---
+++ webpack/webpack.common.js
@@ -0,0 +1,67 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+
+const ROOT_DIR = path.resolve(__dirname, '../');
+
+module.exports = {
+  context: path.resolve(ROOT_DIR, 'src'),
+  entry: './index.ts',
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(ROOT_DIR, 'dist'),
+    clean: true,
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(ROOT_DIR, 'public/index.html'),
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(ROOT_DIR, 'public/assets'),
+          to: path.resolve(ROOT_DIR, 'dist/assets'),
+        },
+      ],
+    }),
+    new MiniCssExtractPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [require('postcss-preset-env')],
+              },
+            },
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.[tj]sx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+}
